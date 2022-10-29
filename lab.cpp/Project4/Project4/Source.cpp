5b9a75b14a16cb95e6b93a8a5424ddafeb1f17a1
VasyaSemenov <116298465+VasyaSemenov@users.noreply.github.com>
2022-10-30 01:40:57 +0500

    Add files via upload

---
+++ lab.cpp/Project4/Project4/Source.cpp
@@ -0,0 +1,56 @@
+#include <istream>
+#include <stdlib.h>
+using namespace std;
+#include<iostream>
+
+
+int main() {
+	setlocale(LC_ALL, "ru");
+	int n, m, k, a;
+	int* mas1, * new_mas;
+	cout<< "Ведите размер масива n";
+	cin >> n;
+	if (n < 0) {
+		while (n < 0) {
+			cout << "Введите другое число"<<endl;
+			cin >> n;
+		}
+	}
+	mas1 = new int[n];
+	cout << "mas="<<endl;
+	for (int i = 0; i < n; i++)
+	{
+		mas1[i] = rand() % 100;
+		cout << mas1[i] << ' ';
+	}
+	k = 0;
+	m = 0;
+	do {
+		cout << "Введите номер с которого добавить"<<endl;
+		cin >> k;
+
+	} while ((k <= 0) && (k > n + 1));
+	do {
+		cout << "Введите количество элеменов"<<endl;
+		cin >> m;
+	} while (m <= 0);
+	a = n + m;
+	new_mas = new int[a];
+	for (int i = 0; i < k; i++) {
+		new_mas[i] = mas1[i];
+	}
+	for (int i = 0, j = k - 1; i < m; j++, i++) {
+		cout << "Введите число"<<endl;
+		cin >> new_mas[j];
+	}
+	for (int i = (k-1+m), j = k - 1; j < n; j++, i++) {
+				new_mas[i] = mas1[i];
+			}
+		cout << "mas2=";
+		for (int i = 0; i < a; i++) {
+			cout << new_mas[i] << " ";
+		}
+	delete[]mas1;
+
+	return(0);
+}
